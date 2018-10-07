a13a00a790d991e66b4eed4260d6945f43da64ff
liqiwei <liqiwei@iastate.edu>
2018-10-07 05:03:04 -0500

    Changed dockerfile

--- react-display/src/App.js
+++ react-display/src/App.js
@@ -3,10 +3,25 @@ import Display from "./components/display";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [
+        {
+          name: "Temperture",
+          data: "33"
+        },
+        {
+          name: "Pressure",
+          data: "300"
+        }
+      ]
+    };
+  }
   render() {
     return (
       <div className="">
-        <Display />
+        <Display data={this.state.data} />
       </div>
     );
   }
--- react-display/src/components/display.jsx
+++ react-display/src/components/display.jsx
@@ -2,25 +2,20 @@ import React, { Component } from "react";
 
 export default class Display extends Component {
   renderList() {
-      //[{name:data}]
-      return this.props.data.map()
+    //[{name:data}]
+    return this.props.data.map(item => {
+      return (
+        <li key={item.name} className="mdl-list__item">
+          <h5>{item.name}:</h5>
+          <h5 className="mdl-list__item-primary-content">{item.data}</h5>
+        </li>
+      );
+    });
   }
   render() {
     return (
       <div>
-        <ul className="demo-list-item mdl-list">
-          <li className="mdl-list__item">
-            <span className="mdl-list__item-primary-content">
-              Bryan Cranston
-            </span>
-          </li>
-          <li className="mdl-list__item">
-            <span className="mdl-list__item-primary-content">Aaron Paul</span>
-          </li>
-          <li className="mdl-list__item">
-            <span className="mdl-list__item-primary-content">Bob Odenkirk</span>
-          </li>
-        </ul>
+        <ul className="demo-list-item mdl-list">{this.renderList()}</ul>
       </div>
     );
   }
