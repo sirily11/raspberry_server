76adb48712b046cbdbaba846f4222e0f99c97b25
liqiwei <liqiwei@iastate.edu>
2018-10-06 20:33:51 -0500

    First commit

---
+++ python-server/app.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template
+from sense_hat import SenseHat
+
+app = Flask(__name__,
+                    template_folder="../react-display/build",
+                    static_folder="../react-display/build/static")
+
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+
+@app.route('/update')
+def update_handler():
+   pass
+
+if __name__ == '__main__':
+    app.run(port=8765,debug=True)
